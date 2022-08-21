b55de62a43aec1ffa9cf3c884c59bb0a626df7f6
ahmedwagd <ahmedwa122@gmail.com>
2022-08-21 13:35:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Website link [NFT Market Place](https://crimson-waterfall-5333.on.fleek.co/).
---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as FocusedStatusBar } from './FocusedStatusBar';
+export { default as HomeHeader } from './HomeHeader';
+export { default as NFTCard } from './NFTCard';
+export { default as DetailsDesc } from './DetailsDesc';
+export { default as DetailsBid } from './DetailsBid';
+export { EndDate, EthPrice, ImageCmp, NFTTitle, People, SubInfo } from './SubInfo';
+export { CircleButton, RecButton } from './Button'
